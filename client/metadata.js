89653c774fadd6c5a23a119a5712db741449d735
Andrew Shell <andrew@andrewshell.org>
2021-12-28 13:08:49 -0600

    Updating display and presaveHandler

--- client/metadata.js
+++ client/metadata.js
@@ -10,15 +10,27 @@
   }
 
   function emit($item, item) {
-    return $item.append(`
-      <p style="background-color:#eee;padding:15px;">
-        ${expand(item.text)}
-      </p>`)
+    if (!$("link[href='/plugins/metadata/metadata.css']").length) {
+      $('<link rel="stylesheet" href="/plugins/metadata/metadata.css" type="text/css">').appendTo("head");
+    }
+
+    const metadata = item.metadata || {};
+    const content = Object.keys(metadata).map((key) => {
+      const value = JSON.stringify(metadata[key]);
+      return `<dl><dt>${key}</dt><dd>${value}</dd></dl>`;
+    }).join(`\n`);
+
+    return $item.append(content)
+  }
+
+  function handlePresave(item, value) {
+    item.metadata = JSON.parse(value);
+    return value;
   }
 
   function bind($item, item) {
     return $item.dblclick(() => {
-      return wiki.textEditor($item, item);
+      return wiki.textEditor($item, item, { presave: handlePresave });
     })
   }
 
